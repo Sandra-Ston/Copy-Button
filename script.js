9390fdeb58c08f2fc7a043b69866d37fa3817d1f
Pietu-pertrauka <stonyte.san@gmail.com>
2024-11-24 20:38:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector(".btn")
+const input = document.querySelector("input")
+
+btn.addEventListener("click",(e)=>{
+e.preventDefault();
+navigator.clipboard.writeText(input.value).then(()=>{
+    btn.innerText = "Copied"
+    setTimeout(()=>{
+        btn.innerText="Copy"
+    }, 2000)
+})
+
+
+
+})
